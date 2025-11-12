36a89d4205dca7f6769916d9595adbd67db49d45
zbybr <buptzbybr@outlook.com>
2025-11-12 10:13:45 +0800

    update

--- test/chain-of-verification-main/run.sh
+++ test/chain-of-verification-main/run.sh
@@ -1,3 +1,3 @@
-python src/main.py --question "Who led the first European expedition to America?" --base_response "Christopher Columbus led the first European expedition to America."
+python src/main.py --question "Hardley Flood is an area of lagoons that support populations of the waterfowl that are a medium sized version of a duck." --base_response "It is a medium-sized version of a goose." --model_key gpt-5
 
 python src/main.py --dataset_path ..\dataset20250926_hallucination_entity.csv --model_key gpt-4o
