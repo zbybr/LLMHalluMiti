4d483053e19fe6db03fdd7c3dec6853da8863edd
zbybr <buptzbybr@outlook.com>
2025-11-12 17:20:03 +0800

    update

--- test/stat/figure.py
+++ test/stat/figure.py
@@ -1,26 +1,50 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['ps.fonttype'] = 42
+mpl.rcParams['figure.dpi'] = 100
+mpl.rcParams['savefig.dpi'] = 100
+mpl.rcParams['savefig.bbox'] = 'tight'
+mpl.rcParams['savefig.pad_inches'] = 0
 
-labels = ['Category 1', 'Category 2', 'Category 3']
-group1 = [20, 34, 30]
-group2 = [25, 32, 34]
+labels = ['gpt-4o', 'gpt-5']
+group1 = [20, 34]
+group2 = [25, 32]
+group3 = [25, 32]
+# groups = [group1, group2, group3]
+# colors = ['orange', 'green', 'purple']
+# labels_group = ['CoT', 'CoVe', 'Our Approach']
 
 x = np.arange(len(labels))
-width = 0.35
+width = 0.2
 
 fig, ax = plt.subplots(figsize=(6, 6))
 
-rects1 = ax.bar(x - width/2, group1, width, label='Group 1', color='coral')
-rects2 = ax.bar(x + width/2, group2, width, label='Group 2', color='steelblue')
+rec1 = ax.bar(x - width, group1, width, label='CoT', color='orange')
+rec2 = ax.bar(x, group2, width, label='CoVe', color='green')
+rec3 = ax.bar(x + width, group3, width, label='Our Approach', color='purple')
 
-ax.set_ylabel('Values')
-ax.set_title('Grouped Bar Chart')
+ax.set_ylabel('Percentage (%)')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
-ax.legend()
+ax.set_title('Recheck Hallucination Rate')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
 
-for rects in [rects1, rects2]:
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+ax.legend(
+    loc='upper center',
+    bbox_to_anchor=(0.5, -0.05),
+    ncol=3,
+    handlelength=1.2,
+    columnspacing=1.5,
+)
+
+for rects in [rec1, rec2, rec3]:
     for rect in rects:
         height = rect.get_height()
         ax.annotate(f'{height}',
@@ -28,5 +52,6 @@ for rects in [rects1, rects2]:
                     xytext=(0, 3),
                     textcoords="offset points",
                     ha='center', va='bottom')
-# plt.savefig('bar_chart_square.png', dpi=600, bbox_inches='tight', pad_inches=0)
+
+# plt.savefig('bar_chart_square.png')
 plt.show()
