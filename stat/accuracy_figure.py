3e04c344e1a13fa346fb7ef935e1734b7fed3d05
zbybr <buptzbybr@outlook.com>
2025-12-09 19:54:16 +0800

    Move and update sample response CSV files
    Renamed and relocated several sample response CSV files to the outputs/ directory for consistency. Updated contents of gpt-5_outputs_gpt-5_sample_responses.csv and gpt-5_vs_outputs_gpt-5_sample_responses.csv. Added new presentation slide (slides/20251210.pptx) and updated statistical figures and scripts in the stat/ directory.

--- stat/accuracy_figure.py
+++ stat/accuracy_figure.py
@@ -10,12 +10,13 @@ mpl.rcParams['savefig.bbox'] = 'tight'
 mpl.rcParams['savefig.pad_inches'] = 0
 
 labels = ['gpt-4o']
-group0 = [26.53]
-group1 = [20.41]
-group2 = [30.61]
-group3 = [69.39]
-group4 = [67.35]
-group5 = [79.59]
+group0 = [31.08]
+group1 = [27.03]
+group2 = [31.08]
+group3 = [62.16]
+group4 = [62.16]
+group5 = [65.33]
+group6 = [72.97]
 
 # groups = [group1, group2, group3]
 # colors = ['orange', 'green', 'purple']
@@ -26,14 +27,15 @@ width = 0.1
 
 fig, ax = plt.subplots(figsize=(7.5, 7.5))
 # x = np.array([0.0, 0.6])
-n_groups = 6
+n_groups = 7
 offsets = np.linspace(-(n_groups-1)/2, (n_groups-1)/2, n_groups) * width
 rec0 = ax.bar(x + offsets[0], group0, width, label='CoT', color='red')
 rec1 = ax.bar(x + offsets[1], group1, width, label='CoVe', color='limegreen')
 rec2 = ax.bar(x + offsets[2], group2, width, label='CoVe - Search Engine', color='cyan')
-rec3 = ax.bar(x + offsets[3], group3, width, label='Our Approach', color='yellow')
-rec4 = ax.bar(x + offsets[4], group4, width, label='Our Approach - mutations', color='darkorange')
-rec5 = ax.bar(x + offsets[5], group5, width, label='pass@6', color='darkviolet')
+rec3 = ax.bar(x + offsets[3], group3, width, label='Our Approach', color='gold')
+rec4 = ax.bar(x + offsets[4], group4, width, label='Our Approach - mut', color='darkorange')
+rec5 = ax.bar(x + offsets[5], group5, width, label='Our Approach - vs', color='mediumorchid')
+rec6 = ax.bar(x + offsets[6], group6, width, label='pass@6', color='lightslategrey')
 
 
 ax.set_ylabel('Accuracy (%)')
@@ -51,12 +53,12 @@ ax.spines['right'].set_visible(False)
 ax.legend(
     loc='upper center',
     bbox_to_anchor=(0.5, -0.05),
-    ncol=3,
+    ncol=4,
     handlelength=1.2,
-    columnspacing=0.9,
+    columnspacing=0.5,
 )
 
-for rects in [rec0, rec1, rec2, rec3, rec4, rec5]:
+for rects in [rec0, rec1, rec2, rec3, rec4, rec5, rec6]:
     for rect in rects:
         height = rect.get_height()
         ax.annotate(f'{height}',
